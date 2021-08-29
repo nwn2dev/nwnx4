1c14fb8e1122028513eb7e6329fc2db21ea64e93
Scott Munday <scottmunday84@gmail.com>
2021-08-29 00:27:20 -0400

    Indented code to be more legible.

--- src/HookHorror/LogService.cpp
+++ src/HookHorror/LogService.cpp
@@ -23,11 +23,11 @@ namespace NWNX4::HookHorror::Log {
         enableSsl = true;
         grpc::SslServerCredentialsOptions::PemKeyCertPair pkcp = { config->GetSslKey(), config->GetSslCertificate() };
         grpc::SslServerCredentialsOptions sslOptions(config->GetVerifySsl() ?
-        GRPC_SSL_REQUEST_CLIENT_CERTIFICATE_AND_VERIFY :
-        GRPC_SSL_REQUEST_CLIENT_CERTIFICATE_BUT_DONT_VERIFY);
+            GRPC_SSL_REQUEST_CLIENT_CERTIFICATE_AND_VERIFY :
+            GRPC_SSL_REQUEST_CLIENT_CERTIFICATE_BUT_DONT_VERIFY);
         sslOptions.pem_root_certs = config->GetHasSslRootCertificates() ?
-                config->GetSslRootCertificates() :
-                "";
+            config->GetSslRootCertificates() :
+            "";
         sslOptions.pem_key_cert_pairs.push_back(pkcp);
         sslCredentials = ::grpc::SslServerCredentials(sslOptions);
     }
