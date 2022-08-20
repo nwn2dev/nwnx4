bd96fa2faf0568ac9c4ed678c43c13787dd79676
Scott Munday <scottmunday84@gmail.com>
2022-08-20 17:54:37 -0400

    Added logstash; slimmed down staged folder

--- docker/nwn2server/docker-entrypoint.sh
+++ docker/nwn2server/docker-entrypoint.sh
@@ -4,8 +4,38 @@ set -xe
 
 if [ ! -f /opt/nwn2_stage/.staged ]
 then
-  cp -nr /opt/nwn2/* /opt/nwn2_stage/
+  pushd /opt/nwn2
+  ls ./
+  cp --parents -v \
+  	Campaigns/Neverwinter\ Nights\ 2\ Campaign*/Campaign.cam \
+  	\
+  	Data/2DA*.zip \
+  	Data/convo*.zip \
+  	Data/Ini*.zip \
+  	Data/lod-merged*.zip \
+  	Data/NWN2_Models*.zip \
+  	Data/scripts*.zip \
+  	Data/SpeedTree*.zip \
+  	Data/Templates*.zip \
+  	Data/walkmesh*.zip \
+  	\
+  	Modules/*.mod \
+  	\
+  	dialog*.TLK \
+  	\
+  	mss32.dll \
+  	NWN2_MemoryMgr.dll \
+  	NWN2_MemoryMgr_amdxp.dll \
+  	\
+  	nwn?.ini \
+  	nwn?player.ini \
+  	nwnpatch.ini \
+  	\
+  	nwn2server.exe \
+  	\
+  	/opt/nwn2_stage/
   touch /opt/nwn2_stage/.staged
+  popd
 fi
 
 exec "$@"
