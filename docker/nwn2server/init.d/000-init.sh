185f6f7aa2635ef88ea6dd65a01b14a82d4b6b4b
Scott Munday <scottmunday84@gmail.com>
2022-08-28 23:56:00 -0400

    Added .env file instead of individual env vars; moved docker-compose.yml to docker folder (Docker files are contained in docker folder only); cleaned up meson to reflect moved files; added /etc/nwn2server/init.d and moved init script for volume bound folders; added both install staging folder and home folder as volumes (easier transfer when saving image)

---
+++ docker/nwn2server/init.d/000-init.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+pushd /opt/nwn2
+
+# Stage data on startup; do not overwrite
+cp --parents -v -n \
+  Campaigns/Neverwinter\ Nights\ 2\ Campaign*/Campaign.cam \
+  \
+  Data/2DA*.zip \
+  Data/convo*.zip \
+  Data/Ini*.zip \
+  Data/lod-merged*.zip \
+  Data/NWN2_Models*.zip \
+  Data/scripts*.zip \
+  Data/SpeedTree*.zip \
+  Data/Templates*.zip \
+  Data/walkmesh*.zip \
+  \
+  dialog*.TLK \
+  \
+  mss32.dll \
+  NWN2_MemoryMgr.dll \
+  NWN2_MemoryMgr_amdxp.dll \
+  nwn2server.exe \
+  \
+  /opt/nwn2_stage/
+
+# Stage all modules and configuration files if there are changes
+cp --parents -v -u \
+  Modules/*.mod \
+  \
+  nwn?.ini \
+  nwn?player.ini \
+  nwnpatch.ini \
+  /opt/nwn2_stage/
+
+popd
