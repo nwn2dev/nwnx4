737d8e9c3a568677404f2152098540e50a78a236
Scott Munday <scottmunday84@gmail.com>
2022-08-29 20:19:31 -0400

    Will only recognize logs from nwnx4 (by default); also cleaned up nwn2 transfer to include Campaigns, Data, Modules, and UI and the vaults

--- docker/nwn2server/init.d/000-init.sh
+++ docker/nwn2server/init.d/000-init.sh
@@ -2,19 +2,15 @@
 
 pushd /opt/nwn2
 
-# Stage data on startup; do not overwrite
-cp --parents -v -n \
-  Campaigns/Neverwinter\ Nights\ 2\ Campaign*/Campaign.cam \
+# Stage data on startup; overwrite if different
+cp --parents -r -u \
+  Campaigns \
+  Data \
+  Modules \
+  UI \
   \
-  Data/2DA*.zip \
-  Data/convo*.zip \
-  Data/Ini*.zip \
-  Data/lod-merged*.zip \
-  Data/NWN2_Models*.zip \
-  Data/scripts*.zip \
-  Data/SpeedTree*.zip \
-  Data/Templates*.zip \
-  Data/walkmesh*.zip \
+  dmvault \
+  Servervault \
   \
   dialog*.TLK \
   \
@@ -22,12 +18,6 @@ cp --parents -v -n \
   NWN2_MemoryMgr.dll \
   NWN2_MemoryMgr_amdxp.dll \
   nwn2server.exe \
-  \
-  /opt/nwn2_stage/
-
-# Stage all modules and configuration files if there are changes
-cp --parents -v -u \
-  Modules/*.mod \
   \
   nwn?.ini \
   nwn?player.ini \
