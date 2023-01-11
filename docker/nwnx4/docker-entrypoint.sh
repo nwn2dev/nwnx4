0c6e62bbad8b0a0538a91ec13ce6c357107d9487
Scott Munday <scottmunday84@gmail.com>
2023-01-11 15:04:50 -0500

    Dropped supervisord.conf and specialized user (muddies water/access).

--- docker/nwnx4/docker-entrypoint.sh
+++ docker/nwnx4/docker-entrypoint.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
-# Set the ownership of all core folders to nwnx4 user/group
-for file in /srv/{nwnx4-user,nwn2-home,nwn2-logs}; do
-  chown nwnx4:nwnx4 -R "$file"
-done
+set -euxo pipefail
 
 # If the NWN2 stage file does not exist in NWN2 install directory, drop
 for file in $(ls /opt/nwn2-stage); do
@@ -14,12 +9,12 @@ for file in $(ls /opt/nwn2-stage); do
   fi
 done
 
-# If the NWN2 install file does not exist in NWN2 stage directory, add and set ownership
+# If the NWN2 install file does not exist in NWN2 stage directory, add
 for file in $(ls /srv/nwn2); do
   if [[ ! -e "/opt/nwn2-stage/$file" ]]; then
     ln -s "/srv/nwn2/$file" /opt/nwn2-stage/
-    chown nwnx4:nwnx4 -h "/opt/nwn2-stage/$file"
   fi
 done
 
+Xvfb $DISPLAY -screen 0 1024x768x16 &
 exec "$@"
