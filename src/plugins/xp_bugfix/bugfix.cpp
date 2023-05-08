d6308f267a075fcc378b1f17c1c7bb986afffa4c
Septirage <75088531+Septirage@users.noreply.github.com>
2023-05-09 01:04:04 +0200

    Update bugfix version.
    With release 1.3 of nwnx4, the new version of bugfix must be 1.0.75.

--- src/plugins/xp_bugfix/bugfix.cpp
+++ src/plugins/xp_bugfix/bugfix.cpp
@@ -33,7 +33,7 @@
 #include <cassert>
 #include <map>
 
-#define BUGFIX_VERSION "1.0.74"
+#define BUGFIX_VERSION "1.0.75"
 #define __NWN2_VERSION_STR(X) #X
 #define _NWN2_VERSION_STR(X) __NWN2_VERSION_STR(X)
 #define NWN2_VERSION _NWN2_VERSION_STR(NWN2SERVER_VERSION)
